4417a92f8ad52c485deef3fe783adc5a085b0646
Zak <super.zak7@gmail.com>
2023-09-05 21:31:41 +0200

    tm

--- modules/twitch_manager.py
+++ modules/twitch_manager.py
@@ -3,6 +3,8 @@ import requests
 import json
 import os
 from app import app
+from twitchio.ext import commands
+
 
 # Load Twitch Configurations
 
@@ -17,12 +19,42 @@ except json.JSONDecodeError:
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
+# Initialize Twitch Bot
+twitch_bot = commands.Bot(
+    irc_token=twitch_configs.get('irc_token', ''),
+    client_id=twitch_configs.get('client_id', ''),
+    nick=twitch_configs.get('nick', ''),
+    prefix=twitch_configs.get('prefix', '!'),
+    initial_channels=[twitch_configs.get('channel', '')]
+)
+
+
+
+async def update_twitch_info(title, category):
+    headers = {
+        'Client-ID': twitch_configs.get('client_id', ''),
+        'Authorization': f"Bearer {twitch_configs.get('oauth_token', '')}"
+    }
+    data = {
+        'title': title,
+        'game_id': category  # You may need to convert category to game_id
+    }
+    response = requests.patch('https://api.twitch.tv/helix/channels', headers=headers, json=data)
+    return response.json()
+
+
+
 # Twitch Pages
 
 
 @app.route('/twitch/home')
 def twitch_home():
-    return render_template('twitch_manager/home.html')
+    # Your code to get Twitch stream title, category, and live status
+    title = "Your Stream Title"
+    category = "Your Stream Category"
+    live_status = "Live or Offline"
+    return render_template('twitch_manager/home.html', title=title, category=category, live_status=live_status)
+
 
 
 @app.route('/twitch/redeems')
