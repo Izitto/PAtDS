7beb9c0b144e6569115e520d9401fbcc105877cf
Zak <super.zak7@gmail.com>
2023-09-30 18:30:11 +0200

    update

--- run.py
+++ run.py
@@ -1,9 +1,30 @@
 import modules.Control as Control
 from app import app
+import asyncio
+from modules import vtstudio
+import atexit
+
+# Cleanup function
+def cleanup():
+    # Add any cleanup logic here
+    loop.stop()  # Stop the event loop
+
+# Register the cleanup function to be called on exit
+atexit.register(cleanup)
+
 # ###################################################
 # if you don't understand, smoke crack first and try again
 if __name__ == '__main__':
+    # Start the Control logic
     Control.start()
-    app.run(debug=False, port=80, host='0.0.0.0')
-    Control.join()
 
+    # Search for the VTubeStudio API server
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(vtstudio.search_api_server())
+
+    # Start the Flask app
+    app.run(debug=False, port=80, host='0.0.0.0')
+
+    # Join the Control logic
+    Control.join()
