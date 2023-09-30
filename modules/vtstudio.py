05f71c73126408ebf5d82d08d664572ea186a929
Zak <super.zak7@gmail.com>
2023-09-30 20:52:53 +0200

    update

--- modules/vtstudio.py
+++ modules/vtstudio.py
@@ -9,20 +9,29 @@ BASE_URL = ""
 VTS_TOKEN_PATH = "/home/izitto/Desktop/Code/PAtDS/vts_token.txt"
 
 async def search_api_server():
-    # Create a UDP socket
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        s.settimeout(5)
-        try:
-            # Send a broadcast message
-            s.sendto(b'VTubeStudio', ('<broadcast>', 8001))
-            # Receive response
-            data, addr = s.recvfrom(1024)
-            global BASE_URL
-            BASE_URL = f"ws://{addr[0]}:8001"
-            emit_socketio_event('vtstudio_api_server', {'address': BASE_URL})
-        except socket.timeout:
-            emit_socketio_event('vtstudio_error', {'message': 'No response from VTubeStudio API server'})
+    MAX_ATTEMPTS = 10  # Define a maximum number of search attempts
+    attempt = 0
+    global BASE_URL
+    while not BASE_URL and attempt < MAX_ATTEMPTS:
+        # Create a UDP socket
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            s.settimeout(5)
+            try:
+                # Send a broadcast message
+                s.sendto(b'VTubeStudio', ('<broadcast>', 8001))
+                # Receive response
+                data, addr = s.recvfrom(1024)
+                global BASE_URL
+                BASE_URL = f"ws://{addr[0]}:8001"
+                emit_socketio_event('vtstudio_api_server', {'address': BASE_URL})
+            except socket.timeout:
+                attempt += 1
+                emit_socketio_event('vtstudio_error', {'message': f'Attempt {attempt}: No response from VTubeStudio API server'})
+
+    if not BASE_URL:
+        emit_socketio_event('vtstudio_error', {'message': 'Failed to find VTubeStudio API server after multiple attempts'})
+
 
 async def get_available_models():
     if not BASE_URL:
