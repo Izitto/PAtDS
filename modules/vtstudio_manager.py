bc107774f503c8bf9b7f8ead825dbe2af37d8278
Zak <super.zak7@gmail.com>
2023-09-30 18:06:36 +0200

    vt

--- modules/vtstudio_manager.py
+++ modules/vtstudio_manager.py
@@ -4,13 +4,17 @@ import json
 import os
 from app import app
 import modules.vtstudio as vtstudio
+import asyncio
 
 # pages
+import asyncio
+
 @app.route('/vts/home')
-def twitch_home():
-    # Fetch available models (assuming you have a function get_available_models in vtstudio.py)
-    models = vtstudio.get_available_models()
+def vts_home():
+    loop = asyncio.get_event_loop()
+    models = loop.run_until_complete(vtstudio.get_available_models())
     return render_template('vts_manager/home.html', models=models)
+
 
 
 # API ENDPOINTS /vts/api/...
