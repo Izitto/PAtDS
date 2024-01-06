7c184d5a9bde0e9594da98aa2ceb8e8550660eab
Zak <super.zak7@gmail.com>
2024-01-06 18:01:08 +0200

    gunicorn implementation

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import queue
 from time import sleep
 import modules
@@ -7,32 +6,9 @@ from flask import Flask, request, render_template, current_app, jsonify, session
 from flask_cors import CORS
 import logging
 import os
-from flask_webpack import Webpack
 app = Flask(__name__, template_folder='/home/izitto/Desktop/Code/PAtDS/templates',
             static_folder='/home/izitto/Desktop/Code/PAtDS/static', )
 
-Webpack = Webpack()
-params = {
-        'DEBUG': True,
-        'WEBPACK_MANIFEST_PATH': '/home/izitto/Desktop/Code/PAtDS/build/manifest.json'
-    }
+
 CORS(app)
 app.config["SECRET_KEY"] = "secret!"
-app.config.update(params)
-Webpack.init_app(app)
-# Create logs directory if it doesn't exist
-log_dir = os.path.join(app.static_folder, 'logs')
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-# Dynamically create log files for each module
-module_names = ["Control", "api", "pages", "twitch_manager", "overlay_manager", "shared"]  # List of module names in /modules
-for module_name in module_names:
-    log_file_path = os.path.join(log_dir, f"{module_name}.txt")
-    logger = logging.getLogger(module_name)
-    logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler(log_file_path)
-    logger.addHandler(file_handler)
-
-# Now you can use these loggers in your modules to log information
-# For example, in Control.py, you can use logging.getLogger("Control").info("Some log message")
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from app import app
+
 import modules.Control as Control
 import modules.VTS.vtstudio as vtstudio
 import modules.TBOT.bot as bot
-
+from app import app
 # start with gunicorn -c gunicorn_config.py run:app
 # ###################################################
 # if you don't understand, smoke crack first and try again
@@ -28,7 +28,9 @@ if __name__ == '__main__':
     Control.join()
 
 '''
+
 def init_app():
+    app = app
     # Start the Control logic
     Control.start()
     vtstudio_thread = vtstudio.initiate_vtstudio_connection()
